3d9c4dc9e5e747184404a085f879b6a108b836eb
UMK <haga5032@naver.com>
2022-11-07 15:29:35 +0900

    206p

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,105 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        //console.log(devices);
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if(currentCamera.label == camera.label){
+                option.selected = true;
+            }
+            cameraSelect.appendChild(option);
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+/*
+async function getMedia(){
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio : true,
+            video : true,
+        });
+        //console.log(myStream);
+        myFace.srcObject = myStream;
+        await getCameras();
+    }catch(e){
+        console.log(e)
+    }
+}
+*/
+
+async function getMedia(deviceId){
+    const initalConstraints = {
+        audio : true,
+        video : {facingMode : "user"}
+    };
+    const cameraConstraints = {
+        audio : true,
+        video : {deviceId : {exact : deviceId}}
+    };
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia(deviceId ? cameraConstraints : initalConstraints);
+        myFace.srcObject =  myStream;
+        if(!deviceId){
+            await getCameras();
+        }
+    }catch(e){
+        console.log(e)
+    }
+}
+
+
+
+
+getMedia();
+
+function handelMuteClick(){
+   //console.log(myStream.getAudioTracks());
+   myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled))
+   if(!muted){
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    }else{
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+
+function handleCameraClick(){
+   //console.log(myStream.getVideoTracks());
+   myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+   if(!cameraOff){
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }else{
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    }
+}
+
+async function handleCameraChange(){
+  //  console.log(cameraSelect.value);
+  await getMedia(cameraSelect.value);
+}
+
+
+muteBtn.addEventListener("click", handelMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+cameraSelect.addEventListener("input", handleCameraChange);
--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 import http from "http";
-import SocketIO from "socket.io";
-import express from 'express';
-import { Http2ServerRequest } from "http2";
-
+import { Server } from "socket.io";
+import express from "express";
 
 
 const app = express();
-const __dirname = "C:/Users/sjd/Desktop/vueproject/WebRTC/src";
+const __dirname = "C:/Users/sjd/Desktop/vueproject/WebRTC_Camera/src";
 app.set("view engine", "pug");
 app.set("views", __dirname+"/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
+const httpServer = http.createServer(app);
+const wsSever = new Server(httpServer);
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
-Http2ServerRequest.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
